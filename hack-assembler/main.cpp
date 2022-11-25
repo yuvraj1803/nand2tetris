4d2fd2a9996817ca2a653743c729a3347acc7848
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2022-11-26 02:06:17 +0530

    Update main.cpp

--- hack-assembler/main.cpp
+++ hack-assembler/main.cpp
@@ -3,15 +3,35 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include "parser.h"
+#include "symbol_table.h"
+#include "translator.h"
 
 using namespace std;
 
 int main(int argc, char * argv[]){
     if(argc != 2){
-        cout << "incorrect format. expected: hackasm file.asm";
+        cout << "input format: hack-assembler <file-name>" << '\n';
         exit(1);
     }
+    vector<string> args;
+    for(int i=0;i<argc;i++){
+        args.push_back((string) argv[i]);
+    }
+    string file = args[1];
+    parser p(file);
+//    p.displayCode();
+    if(p.fileError){
+        cout << "error opening the file\neither file doesnt exist or doesnt have read permissions" << '\n';
+        exit(1);
+    }
+    string outfile = file.substr(0,file.size()-4);
+    outfile += ".hack";
+    vector<string> mc = p.machineCode;
+    ofstream of(outfile);
+    for(const auto& i:mc) of << i << '\n';
+    of.close();
 
-    string file = argv[1];
-
+    exit(0);
 }
