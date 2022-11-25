ce6bb8dcfbf1f52a98ed39b958c02a40d75896e4
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2022-11-26 02:08:51 +0530

    Update parser.h

--- hack-assembler/parser.h
+++ hack-assembler/parser.h
@@ -5,8 +5,30 @@
 #ifndef HACK_ASSEMBLER_PARSER_H
 #define HACK_ASSEMBLER_PARSER_H
 
+#include <string>
+#include <vector>
+#include <fstream>
+#include "symbol_table.h"
+#include "translator.h"
+
+using namespace std;
 
 class parser {
+private:
+    vector<string> parse();
+    vector<string> code;
+
+    string removeComments(string &line);
+    string removeSpaces(string &line);
+    string integerToBinaryString(int n);
+
+    bool checkEmptyString(string &line);
+
+public:
+     explicit parser(const string& file);
+     bool fileError = false;
+     vector<string> machineCode;
+     void displayCode();
 
 };
 
