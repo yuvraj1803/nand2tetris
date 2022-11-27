50169f0d5c69d8ce1715bb7f390d1e1103ad758f
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2022-11-27 20:38:55 +0530

    Update main.cpp

--- hack-assembler/main.cpp
+++ hack-assembler/main.cpp
@@ -4,31 +4,25 @@
 
 #include <iostream>
 #include <fstream>
-#include "parser.h"
-#include "symbol_table.h"
-#include "translator.h"
+
+#include "Assembler.h"
 
 using namespace std;
 
 int main(int argc, char * argv[]){
     if(argc != 2){
-        cout << "input format: ./hack-assembler <file-name>" << '\n';
+        cout << "input format: hack-assembler <file-name>" << '\n';
         exit(1);
     }
-    vector<string> args;
-    for(int i=0;i<argc;i++){
-        args.push_back((string) argv[i]);
-    }
-    string file = args[1];
-    parser p(file);
-//    p.displayCode();
-    if(p.fileError){
-        cout << "error opening the file\neither file doesnt exist or doesnt have read permissions" << '\n';
-        exit(1);
-    }
+
+    string file = argv[1];
+
+
+    Assembler hack_assembler = *new Assembler(file);
+
     string outfile = file.substr(0,file.size()-4);
     outfile += ".hack";
-    vector<string> mc = p.machineCode;
+    vector<string> mc = hack_assembler.machine_code;
     ofstream of(outfile);
     for(const auto& i:mc) of << i << '\n';
     of.close();
