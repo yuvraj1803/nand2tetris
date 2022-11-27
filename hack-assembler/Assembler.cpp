332b26e8e07316e9d634822278d60a46d6e98052
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2022-11-27 20:28:02 +0530

    Added Assembler class.

---
+++ hack-assembler/Assembler.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Yuvraj Sakshith on 27/11/22.
+//
+
+#include "Assembler.h"
+
+
+Assembler::Assembler(std::string &assembly_file) {
+
+    HACK_PARSER.loadFile(assembly_file);
+    if(HACK_PARSER.fileError){
+        cout << "error loading the file. check the existance and file-permissions of the file" << '\n';
+        return;
+    }
+
+    clean_code = HACK_PARSER.displayCode();
+    machine_code = HACK_PARSER.parse();
+
+}
---
+++ hack-assembler/Assembler.h
@@ -0,0 +1,22 @@
+//
+// Created by Yuvraj Sakshith on 27/11/22.
+//
+
+#ifndef HACK_ASSEMBLER_ASSEMBLER_H
+#define HACK_ASSEMBLER_ASSEMBLER_H
+
+#include "parser.h"
+#include <string>
+
+class Assembler {
+private:
+    parser HACK_PARSER = *new parser();
+public:
+
+    Assembler(string &assembly_file);
+    vector<string> machine_code;
+    vector<string> clean_code;
+};
+
+
+#endif //HACK_ASSEMBLER_ASSEMBLER_H
