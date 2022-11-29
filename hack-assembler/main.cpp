f3f0a8aa49c87dad3047e07fabda14dc6bf90055
Yuvraj Sakshith <37622719+yuvraj1803@users.noreply.github.com>
2022-11-30 00:01:43 +0530

    flag handling included

--- hack-assembler/main.cpp
+++ hack-assembler/main.cpp
@@ -10,13 +10,22 @@
 using namespace std;
 
 int main(int argc, char * argv[]){
-    if(argc != 2){
-        cout << "input format: hack-assembler <file-name>" << '\n';
+
+    if(argc == 3){
+        if(strcmp(argv[1],"-c") != 0){
+            cout << "incorrect flags passed." << '\n';
+            exit(1);
+        }
+
+    }
+    if(argc < 2){
+        cout << "input format: hack-assembler <flags> <file-name>" << '\n';
         exit(1);
     }
 
-    string file = argv[1];
 
+
+    string file = argv[argc-1];
 
     Assembler hack_assembler = *new Assembler(file);
 
@@ -26,6 +35,14 @@ int main(int argc, char * argv[]){
     ofstream of(outfile);
     for(const auto& i:mc) of << i << '\n';
     of.close();
+
+    if(argc == 3 and !strcmp(argv[1],"-c")){
+        int lineNr = 0;
+        for(auto i: hack_assembler.clean_code){
+            cout << lineNr << ':' << i << '\n';
+            lineNr++;
+        }
+    }
 
     exit(0);
 }
